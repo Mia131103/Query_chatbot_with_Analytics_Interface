f5a9b27f5b739436f78ac6d6489a09c278a8a438
Mia131103 <s.mia131103@gmail.com>
2026-07-30 00:44:31 -0700

    SQL bot and Interface added

---
+++ SQL_generator.py
@@ -0,0 +1,215 @@
+from langgraph.graph import StateGraph, END
+from typing import TypedDict, Annotated, List, Any
+import operator
+from langchain_core.messages import AnyMessage, SystemMessage, HumanMessage, ToolMessage, AIMessage
+from langchain_openai import ChatOpenAI
+import os
+from dotenv import load_dotenv, find_dotenv 
+from langgraph.checkpoint.sqlite import SqliteSaver
+from langchain_community.utilities import SQLDatabase
+import clickhouse_connect
+from pydantic import BaseModel
+
+#Loading env and making our tool
+load_dotenv(find_dotenv())
+
+#Loading our database
+db = clickhouse_connect.get_client(
+    host=os.getenv("HOST"),
+    port=int(os.getenv("PORT")),
+    username=os.getenv("USER"),
+    password=os.getenv("PASSWORD"),
+    database=os.getenv("DATABASE"),
+    secure= True
+)
+
+def get_schema(client):
+    tables = client.query("""
+    SELECT
+        table,
+        name,
+        type
+    FROM system.columns
+    WHERE database = currentDatabase()
+    ORDER BY table, position
+    """).result_rows
+    schema = {}
+    for table, column, datatype in tables:
+        schema.setdefault(table, []).append(f"{column} {datatype}")
+    schema_text = ""
+    for table, cols in schema.items():
+        schema_text += f"Table: {table}\n"
+        schema_text += "\n".join(cols)
+        schema_text += "\n\n"
+    return schema_text
+schema = get_schema(db)
+
+class AgentState(TypedDict):
+    user: str
+    sql: str
+    llm_critique: str
+    execute_error: str
+    data: list[dict[str, Any]]
+    regenerations: int
+
+class SQLCheck(BaseModel):
+    valid: bool
+    error: str
+
+# === PROMPT FOR EACH LLM NODE ===
+
+#SQL generating node
+SQL_generation_prompt = """You are an expert SQL generating assistant. Use the database \
+Schema provided in {schema} and the user question and generate SQL accordingly. \
+ONLY generate SELECT statements. \
+
+Return ONLY executable SQL.
+Do not wrap it in markdown.
+Do not explain your answer.
+Do not include ```sql. """
+
+#LLM verification node
+llm_verification_prompt = """
+You are an SQL debugger. \
+
+Check the given SQL generated for the given user question for logical and syntax errors. \
+Make sure it is a SELECT statement, not INSERT, DELETE, UPDATE etc \
+Use the {schema} to check for these errors. \
+
+Do not pick errors over efficiency or be nitpicky. \
+Only give error when it will definitely cause the query to fail or \
+given incorrect results.\
+
+Response should be generated according to the provided structured output.\
+"""
+
+#SQL generation on error
+SQL_generation_error_prompt = """You are an expert SQL generator. You will use the user question,\
+the SQL generated against that question, and the error in that code. Fix the problem in the SQL \
+according to the error given and generate the new SQL code. Use the {schema} to understand the database.\
+ONLY generate SELECT statements. \
+
+Return ONLY corrected executable SQL.
+Do not wrap it in markdown.
+Do not explain your answer.
+Do not include ```sql. """
+
+# === CREATING ALL NODE FUNCTIONS ===
+
+#SQL Gneration node
+def SQL_generation_node(state: AgentState):
+    messages = [
+        SystemMessage(content=SQL_generation_prompt.format(schema=schema)),
+        HumanMessage(content=state['user'])
+    ]
+    response = model.invoke(messages)
+    return {"sql": response.content}
+
+#LLM verification node
+def llm_verification_node(state: AgentState):
+    UserMessage = HumanMessage(
+        content=f"User Question: {state['user']}\nSQL generated: {state['sql']}"
+    )
+    messages = [
+        SystemMessage(content=llm_verification_prompt.format(schema=schema)),
+        UserMessage
+    ]
+    response = model.with_structured_output(SQLCheck).invoke(messages)
+    return {"llm_critique": response.error}
+
+#Execute testing
+def execute_verification_node(state: AgentState):
+    sql = state['sql'].strip().upper()
+    if not sql.startswith("SELECT"):
+        return {"execute_error": "Only SELECT statements are allowed."}
+
+    try:
+        db.query(state['sql'])
+        return {"execute_error": ""}
+    except Exception as e:
+        return {"execute_error": str(e)}
+
+#SQL Regeneration node
+def SQL_regeneration_node(state: AgentState):
+    error = (state["llm_critique"] or state['execute_error'])
+    UserMessage = HumanMessage(
+        content=f"User Question: {state['user']}\n\nGenerated SQL: {state['sql']}\n\nError: {error}"
+    )
+    messages = [
+        SystemMessage(content=SQL_generation_error_prompt.format(schema=schema)),
+        UserMessage
+    ]
+    response = model.invoke(messages)
+    return {
+        "sql": response.content, 
+        "llm_critique": "",
+        "execute_error": "",
+        "regenerations": state.get("regenerations", 1) + 1
+    }
+
+#Fetching the actual data node
+def fetch_data_node(state: AgentState):
+    result = db.query(state["sql"])
+    rows = [
+        dict(zip(result.column_names, row))
+        for row in result.result_rows
+    ]
+    return {"data": rows}
+
+# === CONDITIONAL EDGE FUNCTIONS ===
+
+#Has LLM verified
+def llm_verified(state: AgentState):
+    if state['regenerations'] < 3:
+            return state['llm_critique'] == ""
+    return True
+
+#Has execute verified
+def execute_verified(state: AgentState):
+    if state['regenerations'] < 3:
+        return state['execute_error'] == ""
+    return True
+
+# === BUILDING THE GRAPH ===
+
+builder = StateGraph(AgentState)
+
+builder.add_node("SQL_Generation", SQL_generation_node)
+builder.add_node("LLM_verification", llm_verification_node)
+builder.add_node("Execute_verification", execute_verification_node)
+builder.add_node("SQL_Regeneration", SQL_regeneration_node)
+builder.add_node("Fetch_data", fetch_data_node)
+
+builder.set_entry_point("SQL_Generation")
+builder.add_edge("SQL_Generation", "LLM_verification")
+builder.add_conditional_edges(
+    "LLM_verification",
+    llm_verified,
+    {True: "Execute_verification", False: "SQL_Regeneration"}
+)
+builder.add_conditional_edges(
+    "Execute_verification",
+    execute_verified,
+    {True: "Fetch_data", False: "SQL_Regeneration"}
+)
+builder.add_edge("SQL_Regeneration", "LLM_verification")
+builder.add_edge("Fetch_data", END)
+
+model = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+
+def run_agent(question: str) -> dict:
+    with SqliteSaver.from_conn_string(":memory:") as memory:
+        
+        graph = builder.compile(checkpointer=memory)
+    
+        thread = {"configurable": {"thread_id": "1"}}
+        state = {
+            "user": question,
+            "sql": "",
+            "llm_critique": "",
+            "execute_error": "",
+            "data": [],
+            "regenerations": 0
+        }
+        final_state = graph.invoke(state, thread)
+        return final_state
---
+++ app.py
@@ -0,0 +1,50 @@
+import streamlit as st
+import pandas as pd
+from SQL_generating_bot import run_agent
+
+st.set_page_config(
+    page_title="SQL Analytics Agent",
+    layout="wide"
+)
+st.title("SQL Analytics Agent")
+
+sql_tab, analytics_tab = st.tabs(
+    ["SQL Querry", "Analytics"]
+)
+
+with sql_tab:
+    st.write("SQL Query Page")
+
+with analytics_tab:
+    st.write("Analytics Page")
+
+with sql_tab:
+    question = st.text_area(
+        "Ask your question",
+        placeholder="Example: Show average salary by department"
+    )
+    generate = st.button("Generate SQL")
+
+if generate:
+    with st.spinner("Generating SQL..."):
+        result = run_agent(question)
+
+        st.subheader("Generated SQL")
+        st.code(
+            result["sql"],
+            language = "sql"
+        )
+
+        df = pd.DataFrame(result["data"])
+        st.subheader("Results")
+        st.dataframe(df, use_container_width=True)
+
+        if result["execute_error"]:
+            st.error(result["execute_error"])
+        if result["llm_critique"]:
+            st.error(result["llm_critique"])
+
+with analytics_tab:
+    st.header("Analytics")
+    st.info("Run a SQL query first.")
+
