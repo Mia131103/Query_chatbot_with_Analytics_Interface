4e229c33b9d14de671dede3dbff7e9e132b28944
Mia131103 <s.mia131103@gmail.com>
2026-07-31 05:27:07 -0700

    build_chart function in visualisation.py added

---
+++ visualisations.py
@@ -0,0 +1,38 @@
+import plotly.express as px
+import pandas as pd
+from typing import TypedDict, Any
+from pydantic import BaseModel
+
+class ChartSpecs(BaseModel):
+    type: str
+    x: str
+    y: str
+    title: str
+
+class Analytics_result(TypedDict):
+    title: str
+    figure: Any
+    description: str
+
+def build_chart(df: pd.DataFrame, spec: ChartSpecs):
+    chart_type = spec.type.value.lower().strip()
+    if chart_type == "bar":
+        figure = px.bar(df, x=spec.x, y=spec.y, title=spec.title)
+    elif chart_type == "line":
+        figure = px.line(df, x=spec.x, y=spec.y, title=spec.title)
+    elif chart_type == "scatter":
+        figure = px.line(df, x=spec.x, y=spec.y, title = spec.title)
+    elif chart_type == "histogram":
+        figure = px.histogram(df, x=spec.x, y=spec.y, title=spec.title)
+    elif chart_type == "pie":
+        figure = px.pie(df, names=spec.x, values=spec.y, title=spec.title)
+    elif chart_type == "box":
+        figure = px.pie(df, x=spec.x, y=spec.y, title = spec.title)
+    else: 
+        raise ValueError(f"Unsupported chart type: {spec.type}")
+    return figure
+
+def default_analytics(df: pd.DataFrame):
+    analytics: list[Analytics_result] = []
+
+    return analytics
